042ce14c4a0986abab8ba42adca8de351fc684ed
alvaropsouza <alvaropatricio61@gmail.com>
2021-11-17 16:29:09 -0300

    Feat: adiçao de tsyringe para injeçao de dependencias

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express"
 import swaggerFile from "./swagger.json"
+import "reflect-metadata";
 
 import { router } from "./routes"
 
 import "./database";
+import "./shared/container"
 
 const app = express();
 
